53142cc3906cd23d7ef32bb9d614606b3089b4c7
Pascal COMBES <pascom@orange.fr>
2018-10-13 22:48:15 +0200

    A very simple HTTP server for tests
    Can be started and stopped through context manager

---
+++ test_server.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python
+# Copyright 2018 Pascal COMBES <pascom@orange.fr>
+#
+# This file is part of SolarProd.
+#
+# SolarProd is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# SolarProd is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with SolarProd. If not, see <http://www.gnu.org/licenses/>
+
+import http.server as http
+
+from threading import Thread
+from contextlib import contextmanager
+
+import os
+
+class TestHTTPServer(http.HTTPServer):
+    def __init__(self, address, handler=http.SimpleHTTPRequestHandler):
+        super().__init__(address, handler)
+
+    def start(self, poll_interval=0.5):
+        superObj = super()
+        serverThread = Thread(target=lambda: superObj.serve_forever(poll_interval), name='server')
+        serverThread.start()
+        print('Server started')
+
+    def stop(self):
+        self.shutdown()
+        print('Server stopped')
+
+    @contextmanager
+    def hold(self):
+        try:
+            self.stop()
+            yield None
+        finally:
+            self.start()
+
+if __name__ == '__main__':
+    os.chdir('testdata')
+
+    with TestHTTPServer(('localhost', 2222)) as httpd:
+        print('Server listening on port {}'.format(httpd.server_port))
+        httpd.serve_forever()
+
+
