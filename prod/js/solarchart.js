3de7832d51b82293d360d28b152aad57421065e1
Pascal COMBES <pascom@orange.fr>
2018-11-01 10:22:59 +0100

    Closure for SolarChart

--- prod/js/solarchart.js
+++ prod/js/solarchart.js
@@ -1,157 +1,56 @@
 function SolarChart(root, data) {
-    // Append chart and legend to root element:
-    this.chartRoot = root.append('svg').attr('id', 'chart')
-                                       .classed('framed', true)
-                                       .attr('preserveAspectRatio', 'xMidYMid meet');
-    this.legendRoot = root.append('div').attr('id', 'legend')
-                                        .classed('framed', true);
-
-    // Setup marker:
-    this.chartRoot.append('marker').lower()
-                                   .attr('id', 'arrowhead')
-                                   .attr('viewBox', "0 0 10 10")
-                                   .attr('refX', '0')
-                                   .attr('refY', '5')
-                                   .attr('markerUnits', 'strokeWidth')
-                                   .attr('markerWidth', '16')
-                                   .attr('markerHeight', '8')
-                                   .attr('preserveAspectRatio', 'none')
-                                   .attr('orient', 'auto')
-                                   .append('path').attr('d', 'M 0 0 L 10 5 L 0 10 z')
-                                                  .style('fill', '#000000');
-
-    // Create plot root element:
-    this.plotRoot = this.chartRoot.append('g').attr('transform', "translate(" + this.margins.left + "," + this.margins.top + ")");
-
-    // Create grid:
-    this.yGrid = this.plotRoot.append('g').classed('grid', true);
-
-    // Create axes:
-    this.xAxis = this.plotRoot.append('g').classed('axis', true)
-                                          .attr('id', 'xaxis');
-    this.yAxis = this.plotRoot.append('g').classed('axis', true)
-                                          .attr('id', 'yaxis');
-
-    // Create label:
-    this.xLabel = this.chartRoot.append('g').classed('label', true);
-    this.xLabel.append('text');
-    this.yLabel = this.chartRoot.append('g').classed('label', true);
-    this.yLabel.append('text').attr('transform', 'rotate(-90)');
-
-    // Create legend:
-    this.legend = new SolarLegend(this.legendRoot);
-
-    this.setData(new EmptyData([], '', '', ''));
-}
-
-SolarChart.prototype = {
     // Margins around the chart:
-    margins: {
+    const margins = {
         left: 60,
         right: 20,
         top: 20,
         bottom: 40,
-    },
-    // Change data (and update plot):
-    setData: function(data) {
-        var ow = null;
-        var oh = null;
-        // Remove old plot:
-        if (this.plot !== undefined) {
-            ow = this.plot.width();
-            oh = this.plot.height();
-            data.variable(this.plot.data.variable());
-            data.aggregation(this.plot.data.aggregation());
-            this.plot.remove();
-        }
-        // Create new plot:
-        if (data.isEmpty())
-            this.plot = new EmptyPlot();
-        else if (data.type == SolarData.Type.DAY)
-            this.plot = new LinePlot();
-        else
-            this.plot = new HistPlot();
-        this.plot.init(this.plotRoot, data);
-        // Resize new plot to old plot size:
-        this.plot.resize(ow, oh);
-        // Draw new plot:
-        this.draw();
-    },
-    // Chart resize event:
-    resize: function(w, h) {
-        // Legend width between 175px and 250px
-        var lw = 175 + 75*(w - 6 - 700)/(1000 - 700);
-        if (lw > 250)
-            lw = 250;
-        if (lw < 175)
-            lw = 0;
-        // Chart width:
-        var cw = w - lw - (8*(lw != 0));
+    };
 
-        // Manages legend:
-        if (lw == 0) {
-            this.legendRoot.style('display', 'none');
-        } else {
-            this.legendRoot.style('display', 'inline-block')
-                           .style('width', (lw - 10) + 'px')
-                           .style('height', h + 'px')
-                           .style('margin-left', '8px');
-        }
+    // Append chart and legend to root element:
+    var chartRoot = root.append('svg').attr('id', 'chart')
+                                      .classed('framed', true)
+                                      .attr('preserveAspectRatio', 'xMidYMid meet');
+    var legendRoot = root.append('div').attr('id', 'legend')
+                                       .classed('framed', true);
 
-        this.chartRoot.attr('width', cw)
-                      .attr('height', h);
+    // Setup marker:
+    chartRoot.append('marker').lower()
+                              .attr('id', 'arrowhead')
+                              .attr('viewBox', "0 0 10 10")
+                              .attr('refX', '0')
+                              .attr('refY', '5')
+                              .attr('markerUnits', 'strokeWidth')
+                              .attr('markerWidth', '16')
+                              .attr('markerHeight', '8')
+                              .attr('preserveAspectRatio', 'none')
+                              .attr('orient', 'auto')
+                              .append('path').attr('d', 'M 0 0 L 10 5 L 0 10 z')
+                                             .style('fill', '#000000');
 
-        // Update plot size only after some time:
-        if (this.resizeTimer !== undefined)
-            clearTimeout(this.resizeTimer);
-        this.resizeTimer = setTimeout(() => {
-            this.resizeTimer = undefined;
+    // Create plot root element:
+    var plotRoot = chartRoot.append('g').attr('transform', "translate(" + margins.left + "," + margins.top + ")");
 
-            this.chartRoot.attr('viewBox', "0 0 " + cw + " " + h);
-            this.plot.resize(cw - this.margins.left - this.margins.right, h -  this.margins.top - this.margins.bottom);
-            this.xLabel.attr('transform', "translate(" + ((cw + this.margins.left - this.margins.right) / 2) + "," + (h - 8) + ")");
-            this.yLabel.attr('transform', "translate(20," + ((h + this.margins.top - this.margins.bottom) / 2) + ")");
+    // Create grid:
+    var yGrid = plotRoot.append('g').classed('grid', true);
 
-            this.draw();
-        }, 100);
-    },
-    // Draw the axes (and the grid)
-    draw: function()
-    {
-        if (this.plot.draw()) {
-            this.legend.clear();
-            if (this.plot.legendStyle)
-                this.legend.draw(this.plot.data.aggregation(), this.plot.legendData, this.plot.legendStyle);
-        } else {
-            this.hide();
-            return false;
-        }
+    // Create axes:
+    var xAxis = plotRoot.append('g').classed('axis', true)
+                                    .attr('id', 'xaxis');
+    var yAxis = plotRoot.append('g').classed('axis', true)
+                                    .attr('id', 'yaxis');
 
-        // Draw axes and grid:
-        this.yGrid.lower().call(this.plot.data.yGrid).select('.domain').remove();
-        this.drawAxis(this.xAxis, this.plot.data.xAxis);
-        this.drawAxis(this.yAxis, this.plot.data.yAxis);
-        this.xAxis.attr('transform', "translate(0, " + this.plot.data.yScale(0) + ")");
+    // Create label:
+    var xLabel = chartRoot.append('g').classed('label', true);
+    xLabel.append('text');
+    var yLabel = chartRoot.append('g').classed('label', true);
+    yLabel.append('text').attr('transform', 'rotate(-90)');
 
-        // Draw labels:
-        this.xLabel.select('text').text(this.plot.data.xLabel);
-        this.yLabel.select('text').text(this.plot.data.yLabel());
+    // Create legend:
+    legend = new SolarLegend(legendRoot);
 
-        return true;
-    },
-    // Removes the current axes:
-    hide: function() {
-        this.legend.clear();
-
-        this.xLabel.select('text').text('');
-        this.yLabel.select('text').text('');
-
-        this.yGrid.selectAll('*').remove();
-        this.xAxis.selectAll('*').remove();
-        this.yAxis.selectAll('*').remove();
-    },
     // Draw an axis:
-    drawAxis: function(selection, axis) {
+    var drawAxis = function(selection, axis) {
         // Add axis and get axis domain:
         var domain = selection.raise().call(axis).select('.domain');
 
@@ -170,5 +69,112 @@ SolarChart.prototype = {
             ticks.attr('x1', this.plot.data.xTickCenter());
             ticks.attr('x2', this.plot.data.xTickCenter());
         }
-    },
+    };
+
+    // Change data (and update plot):
+    this.setData = function(data) {
+        var ow = null;
+        var oh = null;
+        // Remove old plot:
+        if (this.plot !== undefined) {
+            ow = this.plot.width();
+            oh = this.plot.height();
+            data.variable(this.plot.data.variable());
+            data.aggregation(this.plot.data.aggregation());
+            this.plot.remove();
+        }
+        // Create new plot:
+        if (data.isEmpty())
+            this.plot = new EmptyPlot();
+        else if (data.type == SolarData.Type.DAY)
+            this.plot = new LinePlot();
+        else
+            this.plot = new HistPlot();
+        this.plot.init(plotRoot, data);
+        // Resize new plot to old plot size:
+        this.plot.resize(ow, oh);
+        // Draw new plot:
+        this.draw();
+    };
+
+    // Chart resize event:
+    this.resize = function(w, h) {
+        // Timer to ensure the chart is not updated continuously:
+        var resizeTimer;
+
+        // Legend width between 175px and 250px
+        var lw = 175 + 75*(w - 6 - 700)/(1000 - 700);
+        if (lw > 250)
+            lw = 250;
+        if (lw < 175)
+            lw = 0;
+        // Chart width:
+        var cw = w - lw - (8*(lw != 0));
+
+        // Manages legend:
+        if (lw == 0) {
+            legendRoot.style('display', 'none');
+        } else {
+            legendRoot.style('display', 'inline-block')
+                      .style('width', (lw - 10) + 'px')
+                      .style('height', h + 'px')
+                      .style('margin-left', '8px');
+        }
+
+        chartRoot.attr('width', cw)
+                 .attr('height', h);
+
+        // Update plot size only after some time:
+        if (resizeTimer !== undefined)
+            clearTimeout(resizeTimer);
+        resizeTimer = setTimeout(() => {
+            resizeTimer = undefined;
+
+            chartRoot.attr('viewBox', "0 0 " + cw + " " + h);
+            this.plot.resize(cw - margins.left - margins.right, h -  margins.top - margins.bottom);
+            xLabel.attr('transform', "translate(" + ((cw + margins.left - margins.right) / 2) + "," + (h - 8) + ")");
+            yLabel.attr('transform', "translate(20," + ((h + margins.top - margins.bottom) / 2) + ")");
+
+            this.draw();
+        }, 100);
+    };
+
+    // Draw the axes (and the grid)
+    this.draw = function()
+    {
+        if (this.plot.draw()) {
+            legend.clear();
+            if (this.plot.legendStyle)
+                legend.draw(this.plot.data.aggregation(), this.plot.legendData, this.plot.legendStyle);
+        } else {
+            this.hide();
+            return false;
+        }
+
+        // Draw axes and grid:
+        yGrid.lower().call(this.plot.data.yGrid).select('.domain').remove();
+        drawAxis.call(this, xAxis, this.plot.data.xAxis);
+        drawAxis.call(this, yAxis, this.plot.data.yAxis);
+        xAxis.attr('transform', "translate(0, " + this.plot.data.yScale(0) + ")");
+
+        // Draw labels:
+        xLabel.select('text').text(this.plot.data.xLabel);
+        yLabel.select('text').text(this.plot.data.yLabel());
+
+        return true;
+    };
+
+    // Removes the current axes:
+    this.hide = function() {
+        legend.clear();
+
+        xLabel.select('text').text('');
+        yLabel.select('text').text('');
+
+        yGrid.selectAll('*').remove();
+        xAxis.selectAll('*').remove();
+        yAxis.selectAll('*').remove();
+    };
+
+    this.setData(new EmptyData([], '', '', ''));
 }
