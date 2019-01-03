b3567e3df8d9373a73468458ac283cad55b42546
Pascal COMBES <pascom@orange.fr>
2019-01-03 10:49:03 +0100

    Moved parser functions to ChartTestCase

--- autotest/tests/chart.py
+++ autotest/tests/chart.py
@@ -27,39 +27,6 @@ class ChartTest(ChartTestCase):
 
         self.lineWidth = 1;
 
-    def parseTranslate(self, group, axis=None):
-        transform = group.get_attribute('transform')
-        match = re.fullmatch(r'translate\(([0-9]*(?:\.[0-9]*)?),([0-9]*(?:\.[0-9]*)?)\)', transform)
-        self.assertIsNotNone(match)
-        if (axis == 'xaxis'):
-            return float(match.group(1))
-        elif (axis == 'yaxis') or (axis == 'grid'):
-            return float(match.group(2))
-        else:
-            return (float(match.group(1)), float(match.group(2)))
-
-    def parseCoords(self, elem, axis):
-        if (axis == 'xaxis'):
-            x = elem.get_attribute('x')
-            if x is not None:
-                return float(x)
-            x1 = elem.get_attribute('x1')
-            x2 = elem.get_attribute('x2')
-            self.assertEqual(x1, x2)
-            if x1 is not None:
-                return float(x1)
-            return 0
-        else:
-            y = elem.get_attribute('y')
-            if y is not None:
-                return float(y)
-            y1 = elem.get_attribute('y1')
-            y2 = elem.get_attribute('y2')
-            self.assertEqual(y1, y2)
-            if y1 is not None:
-                return float(y1)
-            return 0
-
     def getAxis(self, axis):
         chart = self.browser.find_element_by_id('chart')
         if (axis == 'grid'):
@@ -158,22 +125,6 @@ class ChartTest(ChartTestCase):
             setattr(self, axis[0] + 'Map', lambda x: tickLabels[max(0, min(len(tickLabels) - 1, int(1 + im + (x - xm) * (iM - im) / (xM - xm))))] + 1 + im + (x - xm) * (iM - im) / (xM - xm) - max(0, min(len(tickLabels) - 1, int(1 + im + (x - xm) * (iM - im) / (xM - xm)))) - extraPadding)
         else:
             setattr(self, axis[0] + 'Map', lambda x: tickLabels[im] + (x - xm) * (tickLabels[iM] - tickLabels[im]) / (xM - xm))
-
-    def parsePath(self, path):
-        self.assertRegex(path, r'^M([0-9]+(?:\.[0-9]*)?,[0-9]+(?:\.[0-9]*)?)(?:L([0-9]+(?:\.[0-9]*)?,[0-9]+(?:\.[0-9]*)?))*$')
-        coords = [tuple([float(n) for n in c.split(',')]) for c in path[1:].split('L')]
-        return [(self.xMap(c[0]), self.yMap(c[1])) for c in coords]
-
-    def parseRect(self, rect):
-        offset1 = self.parseTranslate(rect.find_element_by_xpath('../..'))
-        offset2 = self.parseTranslate(rect.find_element_by_xpath('..'))
-
-        return (
-            self.xMap(float(rect.get_attribute('x')) + offset1[0] + offset2[0]),
-            self.yMap(float(rect.get_attribute('y')) + offset1[1] + offset2[1]),
-            self.xMap(float(rect.get_attribute('x')) + offset1[0] + offset2[0] + float(rect.get_attribute('width'))),
-            self.yMap(float(rect.get_attribute('y')) + offset1[1] + offset2[1] + float(rect.get_attribute('height')))
-        )
 
     def getLabel(self, axis):
         chart = self.browser.find_element_by_id('chart')
--- autotest/tests/chart_testcase.py
+++ autotest/tests/chart_testcase.py
@@ -15,6 +15,8 @@
 # You should have received a copy of the GNU General Public License
 # along with SolarProd. If not, see <http://www.gnu.org/licenses/
 
+import re
+
 from .helpers import groupby
 from .browser_testcase import BrowserTestCase
 
@@ -34,6 +36,57 @@ class ChartTestCase(BrowserTestCase):
             return float(elem.get_attribute('fill-opacity'))
         else:
             raise ValueError('Unknown element: {}'.format(elem.tag_name))
+
+    def parseTranslate(self, group, axis=None):
+        transform = group.get_attribute('transform')
+        match = re.fullmatch(r'translate\(([0-9]*(?:\.[0-9]*)?), *([0-9]*(?:\.[0-9]*)?)\)', transform)
+        self.assertIsNotNone(match)
+        if (axis == 'xaxis'):
+            return float(match.group(1))
+        elif (axis == 'yaxis') or (axis == 'grid'):
+            return float(match.group(2))
+        else:
+            return (float(match.group(1)), float(match.group(2)))
+
+    def parseCoords(self, elem, axis):
+        if (axis == 'xaxis'):
+            x = elem.get_attribute('x')
+            if x is not None:
+                return float(x)
+            x1 = elem.get_attribute('x1')
+            x2 = elem.get_attribute('x2')
+            self.assertEqual(x1, x2)
+            if x1 is not None:
+                return float(x1)
+            return 0
+        elif (axis == 'yaxis') or (axis == 'grid'):
+            y = elem.get_attribute('y')
+            if y is not None:
+                return float(y)
+            y1 = elem.get_attribute('y1')
+            y2 = elem.get_attribute('y2')
+            self.assertEqual(y1, y2)
+            if y1 is not None:
+                return float(y1)
+            return 0
+        else:
+            raise NotImplementedError()
+
+    def parsePath(self, path):
+        self.assertRegex(path, r'^M([0-9]+(?:\.[0-9]*)?,[0-9]+(?:\.[0-9]*)?)(?:L([0-9]+(?:\.[0-9]*)?,[0-9]+(?:\.[0-9]*)?))*$')
+        coords = [tuple([float(n) for n in c.split(',')]) for c in path[1:].split('L')]
+        return [(self.xMap(c[0]), self.yMap(c[1])) for c in coords]
+
+    def parseRect(self, rect):
+        offset1 = self.parseTranslate(rect.find_element_by_xpath('../..'))
+        offset2 = self.parseTranslate(rect.find_element_by_xpath('..'))
+
+        return (
+            self.xMap(float(rect.get_attribute('x')) + offset1[0] + offset2[0]),
+            self.yMap(float(rect.get_attribute('y')) + offset1[1] + offset2[1]),
+            self.xMap(float(rect.get_attribute('x')) + offset1[0] + offset2[0] + float(rect.get_attribute('width'))),
+            self.yMap(float(rect.get_attribute('y')) + offset1[1] + offset2[1] + float(rect.get_attribute('height')))
+        )
 
     def getLines(self, *args):
         chart = self.browser.find_element_by_id('chart')
